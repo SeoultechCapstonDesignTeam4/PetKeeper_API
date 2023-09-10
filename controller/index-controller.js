6b457263aea6d70ba82fbfd9c9f593eed3b95af1
hchbae1001 <hchbae1001@gmail.com>
2023-09-10 16:45:31 +0900

    Hospital Error 처리리

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -3,29 +3,49 @@ const { default: axios } = require('axios');
 const proj4 = require('proj4');
 
 async function getHostpital(req,res){
-  const {start,end} = req.query;
-  const eps2097 = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs";
-  const wgs84 = "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
-  const url = `http://openapi.seoul.go.kr:8088/${process.env.HOSPITAL_API_KEY}/json/LOCALDATA_020301/${start}/${end}`
-  
-  let data = await axios.get(url);
-  data = data.data['LOCALDATA_020301'];
-  const extractedData = await data.row.map((row) => {
-    const easting = parseFloat(row.X);
-    const northing = parseFloat(row.Y);
-    const eps2097p = proj4(eps2097, wgs84, [easting, northing]);
-    return {
-      NAME: row.BPLCNM,
-      ADDRESS: row.RDNWHLADDR,
-      PHONE: row.SITETEL,
-      X: eps2097p[1],
-      Y: eps2097p[0],
-    };
-  });
-  return res.status(200).json({
-    data:extractedData
-  })
+  try{
+    const {start,end} = req.query;
+    if(start === undefined || end === undefined){
+      throw new Error('start or end is undefined');
+    }else if(start.isNaN || end.isNaN){
+      throw new Error('start or end is NaN');
+    }else if(start < 0 || end < 0){
+      throw new Error('start or end is negative');
+    }else if(start > end){
+      throw new Error('start is bigger than end');
+    }else if(end - start > 1000){
+      throw new Error('end - start is bigger than 1000');
+    }
+    const eps2097 = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs";
+    const wgs84 = "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
+    const url = `http://openapi.seoul.go.kr:8088/${process.env.HOSPITAL_API_KEY}/json/LOCALDATA_020301/${start}/${end}`
+    
+    let data = await axios.get(url);
+    data = data.data['LOCALDATA_020301'];
+    const extractedData = await data.row.map((row) => {
+      const easting = parseFloat(row.X);
+      const northing = parseFloat(row.Y);
+      const eps2097p = proj4(eps2097, wgs84, [easting, northing]);
+      return {
+        NAME: row.BPLCNM,
+        ADDRESS: row.RDNWHLADDR,
+        PHONE: row.SITETEL,
+        X: eps2097p[1],
+        Y: eps2097p[0],
+      };
+    });
+    return res.status(200).json({
+      data:extractedData
+    })
+  }catch(err){
+    return res.status(403).json({
+      success: false,
+      message: err.message
+    })
+  }
 }
+
+
 function index(req, res, next) {
   res.render('index', { title: 'Capstone4Team', name: 'PetKeeper' })
 }
