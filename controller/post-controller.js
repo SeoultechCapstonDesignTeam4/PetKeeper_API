99adf779f5943d654451ba728598baf054cbe120
hchbae1001 <hchbae1001@gmail.com>
2023-09-24 10:14:43 +0900

    Post Controller 시작

---
+++ controller/post-controller.js
@@ -0,0 +1,9 @@
+const postService = require('../service/post-service');
+const {uploadImg,deleteImg} = require('../routes/middle/aws-s3');
+const sharp = require('sharp');
+
+
+
+module.exports={
+
+}
