221dc06ae63e8ab270647f7d990cdd69c2279d36
hchbae1001 <hchbae1001@gmail.com>
2023-09-25 15:08:23 +0900

    Naver OAUTH 구현

--- routes/oauth-route.js
+++ routes/oauth-route.js
@@ -1,11 +1,46 @@
-var express = require('express');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var jwt = require('jsonwebtoken');
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const NaverStrategy = require('passport-naver').Strategy;
+const KakaoStrategy = require('passport-kakao').Strategy;
+const jwt = require('jsonwebtoken');
 
-var router = express.Router();
+const router = express.Router();
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, JWT_SECRET } = process.env;
+const { 
+  NAVER_CLIENT_ID, NAVER_CLIENT_SECRET,NAVER_REDIRECT_URL,
+  GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL,
+  JWT_SECRET } = process.env;
+
+passport.use(new NaverStrategy({
+  clientID: NAVER_CLIENT_ID,
+  clientSecret: NAVER_CLIENT_SECRET,
+  callbackURL: NAVER_REDIRECT_URL,
+  },function (accessToken, refreshToken, profile, cb){
+    let token;
+    console.log(profile);
+    try{
+      token = jwt.sign({
+        user_id: profile.id,
+        email: profile.emails[0].value,
+        name: profile.displayName,
+      }, JWT_SECRET, { expiresIn: '1d' });
+      console.log(token);
+    }catch(err){
+      console.error("Error in NaverStrategy callback:", err);
+      return cb(err);
+    }
+    return cb(null, { profile, token });
+  }
+))
+
+router.get('/naver', passport.authenticate('naver'));
+
+router.get('/naver/callback',passport.authenticate('naver', {
+failureRedirect: '/login',session: false
+}), function(req, res){
+  return res.status(200).json({ token: req.user.token });
+});
 
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
